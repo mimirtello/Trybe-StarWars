6a03d77dd6a595cc6be83deac711f4bd0d5f1bcc
Yasmin Reis Tello <mimirt08@gmail.com>
2022-09-23 12:32:54 -0300

    Requisito de Teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import PlanetProvider from '../context/PlanetProvider';
+import testData from '../../cypress/mocks/testData';
+import fetch from '../../cypress/mocks/fetch';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe ('Carrega tela', () => {
+  it('Testa se chama a api', async() => {
+
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(testData) }));
+
+    render(<PlanetProvider><App /></PlanetProvider>);
+
+    expect(global.fetch).toBeCalled();
+    expect(global.fetch).toBeCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
+
+    // const loading = (screen.findByRole('h1', { name: /Loading.../i }));
+    // await waitFor(() => {
+    //   expect(loading).not.toBeInTheDocument();
+    // });
+
+    const planetaAlderaan = await screen.findByText('Alderaan');
+    expect(planetaAlderaan).toBeInTheDocument();
+  });
+  jest.clearAllMocks();
 });
